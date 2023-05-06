d8d2152e89e6cbd8976fdb32a760820fe233e766
Tadej <tadej.petric1@gmail.com>
2023-05-07 00:37:32 +0200

    finished dynamic version of lanczos, dysfunctional QR lanczos added

--- nal2/nal2.cpp
+++ nal2/nal2.cpp
@@ -13,25 +13,125 @@
 
 typedef Eigen::SparseMatrix<double> smatrix;
 
-using Eigen::Vector;
+using Eigen::VectorXd;
+using Eigen::Vector3d;
 
-template <int n>
-void d_lanczos(smatrix& A, Vector<double, n>& x, Vector<double, n>& b) {
-    Vector<double, n> r = b - A*x;
-    Vector<double, n> v_this = r / r.norm();
-    Vector<double, n> v_last = Vector<double, n>::Zero();
-    Vector<double, n> p;
+bool small(double x) {
+    return (x < 0.00003) && (x > -0.00003);
+
+}
+
+
+void first_iter(smatrix& A, VectorXd& p, double& alpha, double& beta, VectorXd& v_this, VectorXd& v_last) {
+    VectorXd z = A*v_last;
+    alpha = v_last.dot(z);
+    z = z - alpha*v_last;
+
+    beta = z.norm();
+    p = v_last/alpha;
+    v_this = z/alpha;
+}
+
+void second_iter(smatrix& A,
+                 VectorXd& p_this,
+                 VectorXd& p_last,
+                 double& alpha, double& beta, double& zeta,
+                 VectorXd& v_this,
+                 VectorXd& v_last,
+                 Vector3d& col) {
+
+    VectorXd z = A*v_this - beta*v_last;
+    auto [c, s] = givens(alpha, beta);
+
+    alpha = v_this.dot(z);
+    z = z - alpha*v_this;
+
+    p_this = v_this - p_last*(c*beta - s*alpha);
+    p_this /= (s*beta + c*alpha);
+    alpha = s*beta + c*alpha;
+
+    beta = z.norm();
+
+    zeta *= s/c;
+
+    col(0) = -s*beta;
+    col(1) =  c*beta;
+}
+
+
+void qr_d_lanczos(smatrix& A, VectorXd& x, VectorXd& b) {
+    VectorXd r = b - A*x;
+    VectorXd v_this;
+    VectorXd v_last = r / r.norm();
+
+    VectorXd p_k2;
+    VectorXd p_k1;
+    VectorXd p_k;
+
+    double alpha_last, beta_last;
+    double alpha_this, beta_this;
+    double zeta = r.norm();
+
+    Vector3d last_col = Vector3d::Zero();
+
+
+    // compute first manually
+    first_iter(A, p_k2, alpha_last, beta_last, v_this, v_last);
+    x += zeta*p_k2;
+    if (small(beta_last)) return;
+    std::cout << x;
+    second_iter(A, p_k1, p_k2, alpha_last, beta_last, zeta, v_this, v_last, last_col);
+
+    x += zeta*p_k1;
+    if (small(beta_last)) return;
+    std::cout << "\n-\n" << x;
+
+    for (int j = 2; j < 3; ++j) {
+
+        VectorXd z = A*v_this - beta_last*v_last;
+        alpha_this = v_this.dot(z);
+        z = z - alpha_this*v_this;
+        beta_this = z.norm();
+
+        auto [c, s] = givens(alpha_last, beta_last);
+        last_col(1) = c*last_col(1) - s*alpha_this;
+        last_col(2) = s*last_col(1) + c*alpha_this;
+
+        alpha_last = s*beta_last + c*alpha_this;
+
+        p_k = v_this - last_col(1)*p_k1 - last_col(0)*p_k2;
+        p_k /= last_col(2);
+
+        zeta *= s/c;
+        std::cout << "\n-\n";
+        std::cout << x;
+        std::cout << "-\n";
+        x += zeta*p_k;
+
+        last_col(0) = -s*beta_this;
+        last_col(1) =  c*beta_this;
+
+        v_last = v_this;
+        v_this = z/beta_this;
+    }
+}
+
+
+void d_lanczos(smatrix& A, VectorXd& x, VectorXd& b) {
+    VectorXd r = b - A*x;
+    VectorXd v_this = r / r.norm();
+    VectorXd v_last = VectorXd::Zero(r.size());
+    VectorXd p = VectorXd::Zero(r.size());
     double beta_this;
     double beta_last = 0;
     double zeta = r.norm();
 
     double lambda = 0;
     double eta = 0;
-    const int i = 5;
 
     for (int j = 0; j < 5000; ++j) {
 
-        Vector<double, n> z = A*v_this - beta_last*v_last;
+        VectorXd z = A*v_this - beta_last*v_last;
         double alpha = v_this.dot(z);
         z = z - alpha*v_this;
 
@@ -41,17 +141,17 @@ void d_lanczos(smatrix& A, Vector<double, n>& x, Vector<double, n>& b) {
         }
         eta = alpha - beta_last*lambda;
         if (eta < 0.00003 && eta > -0.00003) {
-            std::cout << "eta";
             break;
         }
         if (j >= 1) {
             zeta = -lambda * zeta;
         }
+
+        // beta(k-1) p(k-1) + eta(k)p(k) = v(k)
         p = (1/eta)*(v_this - beta_last*p);
         x += zeta*p;
 
         if (beta_this < 0.00003 && beta_this > -0.00003) {
-            std::cout << "beta_this";
             break;
         }
         if (std::abs(beta_this * zeta/eta) < std::pow(10.,-10.))
@@ -63,45 +163,34 @@ void d_lanczos(smatrix& A, Vector<double, n>& x, Vector<double, n>& b) {
     }
 }
 
+
+
 int main(int argc, char** argv) {
-    /*bool s = false;
-    smatrix mat;
+    bool s = false;
+
+    smatrix A;
+    VectorXd b;
+    
     if (argc <= 1) {
         char opt = '1';
-        load_matrix(mat, &opt);
+        load_matrix(A, b, &opt);
     } else {
-        load_matrix(mat, argv[1]);
+        load_matrix(A, b,  argv[1]);
     }
-    mat.makeCompressed();
-    std::cout << mat.rows() << " " << mat.cols();
+    A.makeCompressed();
+    std::cout << A.rows() << " " << A.cols() << "\n";
+    std::cout << b.rows() << " " << b.cols() << "\n";
 
-    for (int i = 0; i < 100; ++i) {
-        for (int j = 0; j < i; ++j) {
-        }
-    }*/
-    srand(0);
-    const int i1 = 5;
+    VectorXd x = VectorXd::Zero(A.rows());
 
-    Eigen::Matrix<double, i1, i1> x1 = Eigen::Matrix<double,i1,i1>::Random();
-    x1 = x1 + x1.transpose().eval();
-    smatrix A(i1, i1);
-    for (int j = 0; j < i1; ++j) {
-        for (int i = 0; i < i1; ++i) {
-            //if (i == j) A.insert(i,i) = i+1;
-            A.insert(i,j) = x1(i,j);
-        }
-    }
-    Vector<double, i1> x = Vector<double, i1>::Zero();
-    Vector<double, i1> b = Vector<double, i1>::Ones();
-    d_lanczos(A, x, b);
-    std::cout << A << "\n";
-    //std::cout << x;
-    std::cout << "\n-----\n";
+
+    qr_d_lanczos(A, x, b);
+
     Eigen::SimplicialLDLT<smatrix> solver;
     solver.compute(A);
-    Vector<double, i1> y = solver.solve(b);
+    VectorXd y = solver.solve(b);
     //std::cout << y;
-    Eigen::Matrix<double, i1, 2> all;
+    Eigen::MatrixXd all(x.size(),2);
     all.col(0) = x;
     all.col(1) = y;
     std::cout << all;
--- nal2/nal2.h
+++ nal2/nal2.h
@@ -8,21 +8,50 @@
 #include <iostream>
 #include <unsupported/Eigen/SparseExtra>
 #include <Eigen/Sparse>
+#include <utility>
+#include <cmath>
 
-using Eigen::SparseMatrix;
+typedef Eigen::SparseMatrix<double> smatrix;
+using Eigen::VectorXd;
 
-bool load_matrix(SparseMatrix<double>& matrix, const char* file) {
+template <int n>
+void make_sample(smatrix& A) {
+    srand(0);
+
+    A.resize(n,n);
+    Eigen::Matrix<double, n, n> x1 = Eigen::Matrix<double,n,n>::Random();
+    x1 = x1 + x1.transpose().eval();
+    for (int j = 0; j < n; ++j) {
+        for (int i = 0; i < n; ++i) {
+            if (i == j) A.insert(i,i) = i+1;
+            //A.insert(i,j) = x1(i,j);
+        }
+    }
+}
+
+void load_matrix(smatrix& matrix, VectorXd& b, const char* file) {
+
     std::string data_location = "../nal2/data/";
     std::string filename = "ncvxbqp1.mtx"; //"data/ncvxbqp1.mtx";
     if (file[0] == '2') filename = "spmsrtls.mtx";
     if (file[1] == '3') filename = "wing.mtx";
+
     filename = data_location + filename;
 
+    if (file[0] == '4') make_sample<10>(matrix);
+    else
+        Eigen::loadMarket(matrix, filename);
 
-
-    auto x = Eigen::loadMarket(matrix, filename);
-    return x;
+    b = VectorXd::Ones(matrix.rows());
 }
+
+std::tuple<double, double> givens(double& alpha, double& beta) {
+    double r = alpha*alpha + beta*beta;
+    r = std::sqrt(r);
+
+    return {alpha/r, -beta/r};
+}
+
 
 
 #endif //NLA_NAL2_H
